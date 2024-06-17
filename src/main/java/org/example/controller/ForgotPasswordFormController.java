5c6d2ed6837da0c4182e391c46705c3ebc328beb
nemsi <nemsithbaduge215@gmail.com>
2024-06-17 15:03:46 +0530

    First Commit

---
+++ src/main/java/org/example/controller/ForgotPasswordFormController.java
@@ -0,0 +1,20 @@
+package org.example.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class ForgotPasswordFormController{
+    public TextField txtEmail;
+    public JFXButton btnSendOtp;
+    public TextField txtNewPassword;
+    public TextField txtOtpCode;
+    public TextField txtReEnterPassword;
+    public JFXButton btnReset;
+
+    public void btnSendOtpOnAction(ActionEvent actionEvent) {
+    }
+
+    public void btnResetOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/main/java/org/example/controller/LoginFormController.java
@@ -0,0 +1,11 @@
+package org.example.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class LoginFormController {
+
+}
